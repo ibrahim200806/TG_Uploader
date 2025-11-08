28ebe65eaa22d5491d6ef9c58adf2d09e100582c
Mohammed Ibrahim M <mohammedibrahimm24it@psnacet.edu.in>
2025-11-08 11:54:09 +0530

    Update gen_sess.py

--- gen_sess.py
+++ gen_sess.py
@@ -6,6 +6,7 @@ except Exception as e:
     exit(1)
 
 print("Required pyrogram V2 or greater.")
+print("Required to add phone number don't use bot token")
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
 with Client(name="USS", api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
